3adc33973fe8739bf87335c78a3c00760ac42301
“dipta”-bit <bantalnolep@gmail.com>
2026-06-11 15:27:45 +0700

    frontend and backend integration

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx'; 
 import './index.css';
 import { ThemeContextProvider } from "./context/themeContext.jsx";
-import Form from "./latihan/Form.jsx";
 import { AuthContextProvider } from './context/authContext.jsx';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
